b1b360ef63df7e675890c8e6596e15e0e8afea92
Srikara Thikkireddy <130203062+srik4442@users.noreply.github.com>
2023-10-14 23:43:26 +0530

    Update Education.js

--- src/components/Education.js
+++ src/components/Education.js
@@ -6,12 +6,12 @@ const Education = () => {
     <section>
                 <h2>Education</h2>
             
-                <h3>Narayana School, Bangalore</h3>
+                <h3>Sri Chaitanya School, Vizag</h3>
                 <p>10th standard</p>
                 <p>Graduation Year: 2019</p>
             
             
-                <h3>Narayana,Bangalore </h3>
+                <h3>Sri Chaitanya,Vizag </h3>
                 <p>Intermediate</p>
                 <p>Graduation Year: 2021</p>
             
